5c62d4a3c1958ec646b34474ae55ed62c50df899
snyamson <snyamson@st.knust.edu.gh>
2023-08-23 15:02:02 +0000

    feat: Implement function to fetch weather details

--- api/endpoints/ip_information.py
+++ api/endpoints/ip_information.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 import httpx
+from dotenv import load_dotenv
+from os import getenv
 
 router = APIRouter()
+
+# Load environment variables from a .env.local file
+load_dotenv('.env.local')
+API_KEY = getenv('OPENWEATHER_API_KEY')
+
 
 
 @router.get("/ip_information")
@@ -10,7 +17,24 @@ async def get_ip_information():
         async with httpx.AsyncClient() as client:
             response = await client.get("https://ipapi.co/json/")
             if response.status_code == 200:
-                return {"ip": response.json()["ip"]}
+                ip_info = response.json()
+                lon = ip_info.get("longitude")
+                lat = ip_info.get("latitude")
+
+                # Fetch weather information
+                weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+
+                weather_response = await client.get(weather_url)
+                if weather_response.status_code == 200:
+                    weather_info = weather_response.json()
+                    return {
+                        "region": ip_info.get("region"),
+                        "country": ip_info.get("country_name"),
+                        "temperature": weather_info["main"].get("temp"),
+                        "humidity": weather_info["main"].get("humidity"),
+                    }
+                else:
+                    return {"error": "Failed to fetch weather information"}
             else:
                 return {"error": "Failed to fetch IP information"}
     except httpx.HTTPError as e:
